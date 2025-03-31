04833ff4489711983149cbfc9b98a0f627a3aa09
YJLEE <dldbwjd24@gmail.com>
2025-03-31 15:20:29 +0900

    01. Composable - Row & Columns
    01. Composable - Row & Columns

--- app/src/main/java/yj/practice/unitconverter/MainActivity.kt
+++ app/src/main/java/yj/practice/unitconverter/MainActivity.kt
@@ -1,9 +1,12 @@
 package yj.practice.unitconverter
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,18 +17,33 @@ import androidx.compose.ui.tooling.preview.Preview
 import yj.practice.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             UnitConverterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { padding ->
+                    UnitConverter()
                 }
             }
+        }
+    }
+}
+
+
+@Composable
+fun UnitConverter(){
+    Column { // column have row
+        //Here all the UI elements will be stacked below each other ( element를 추가하면 세로방향으로 stack )
+        Row {
+            //Here all the UI elements will be stacked below each other ( element를 추가하면 가로방향으로 stack )
+            Greeting(
+                name = "Android"
+            )
+            Greeting(
+                name = "Android"
+            )
         }
     }
 }
