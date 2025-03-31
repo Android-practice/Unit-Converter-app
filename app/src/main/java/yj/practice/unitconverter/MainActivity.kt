39909cf9a48cd788f43c0c579343a1a059cd3de0
YJLEE <dldbwjd24@gmail.com>
2025-03-31 16:14:22 +0900

    Button

--- app/src/main/java/yj/practice/unitconverter/MainActivity.kt
+++ app/src/main/java/yj/practice/unitconverter/MainActivity.kt
@@ -2,6 +2,7 @@ package yj.practice.unitconverter
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,10 +10,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import yj.practice.unitconverter.ui.theme.UnitConverterTheme
 
@@ -33,33 +37,35 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun UnitConverter(){
+fun UnitConverter() {
     Column { // column have row
         //Here all the UI elements will be stacked below each other ( element를 추가하면 세로방향으로 stack )
+        Text("Unit Converter")
+        Text("Unit Converter")
+        OutlinedTextField(value = "", onValueChange = {
+            //Here goes what should happen, when the value of our OutLinedTextField changes
+        })
         Row {
+            //context : which area of application should something happen?
+            val context = LocalContext.current
             //Here all the UI elements will be stacked below each other ( element를 추가하면 가로방향으로 stack )
-            Greeting(
-                name = "Android"
-            )
-            Greeting(
-                name = "Android"
-            )
+            Button(onClick = {
+                Toast.makeText(
+                    context,
+                    "Thanks for clicking!",
+                    Toast.LENGTH_LONG
+                ).show()
+            }) {
+                Text("Click me!")
+            }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UnitConverterTheme {
-        Greeting("Android")
-    }
+fun UnitConverterPreview() {
+    UnitConverter()
 }
