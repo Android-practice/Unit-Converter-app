3c8ef4b5a2e4a931cff85ef1a35a1a4bb8511cb5
YJLEE <dldbwjd24@gmail.com>
2025-03-31 16:28:52 +0900

    Box

--- app/src/main/java/yj/practice/unitconverter/MainActivity.kt
+++ app/src/main/java/yj/practice/unitconverter/MainActivity.kt
@@ -6,11 +6,15 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -41,25 +45,24 @@ fun UnitConverter() {
     Column { // column have row
         //Here all the UI elements will be stacked below each other ( element를 추가하면 세로방향으로 stack )
         Text("Unit Converter")
-        Text("Unit Converter")
         OutlinedTextField(value = "", onValueChange = {
             //Here goes what should happen, when the value of our OutLinedTextField changes
         })
         Row {
-            //context : which area of application should something happen?
-            val context = LocalContext.current
-            //Here all the UI elements will be stacked below each other ( element를 추가하면 가로방향으로 stack )
-            Button(onClick = {
-                Toast.makeText(
-                    context,
-                    "Thanks for clicking!",
-                    Toast.LENGTH_LONG
-                ).show()
-            }) {
-                Text("Click me!")
+            Box {
+                Button(onClick = {}){
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
             }
-
+            Box {
+                Button(onClick = {}){
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
+            }
         }
+        Text("Result : ")
     }
 }
 
